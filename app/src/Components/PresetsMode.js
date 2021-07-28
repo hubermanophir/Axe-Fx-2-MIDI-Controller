0e3fb482bf6d28b3741fa1092253b7823ef1d4be
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-07-28 18:19:41 +0300

    Fixed events when dialog is open preset change is disabled

--- app/src/Components/PresetsMode.js
+++ app/src/Components/PresetsMode.js
@@ -1,13 +1,42 @@
 import { Button } from "@material-ui/core";
+import { useData } from "../Context/AppContext";
+import { setPreset } from "../Utils/midiFunctions";
 import React, { useEffect, useState } from "react";
 import Preset from "./Preset";
-import presets from "../UserData/presets";
+import presets from "../UserData/presets.json";
+import FormDialog from "./FormDialog";
 
 export default function PresetsMode() {
+  const { device } = useData();
+
+  const presetEventListener = async (e) => {
+    function findWithAttr(array, attr, value) {
+      for (let i = 0; i < array.length; i += 1) {
+        if (array[i][attr] === value) {
+          return array[i];
+        }
+      }
+      return -1;
+    }
+
+    const preset = findWithAttr(presets, "key", e.key);
+    if (preset !== -1) {
+      await setPreset(preset.presetNumber, 1, device.id);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keyup", presetEventListener, true);
+    return () => {
+      window.removeEventListener("keyup", presetEventListener, true);
+    };
+  }, []);
+
   return (
     <div>
       <h1>Presets</h1>
       {presets && presets.map((preset) => <Preset preset={preset} />)}
+      <FormDialog presetEventListener={presetEventListener} />
     </div>
   );
 }
